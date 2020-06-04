1cf0145e34621441c22944533672b0104abcf12d
koji <kk2796@nyu.edu>
2020-06-04 15:39:50 -0400

    Update webpack.config.js

--- webpack_typescript_react/webpack.config.js
+++ webpack_typescript_react/webpack.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 const autoprefixer = require('autoprefixer');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
