b8c71747fe72521fbc605b576a737a18dfaa52ff
koji <kk2796@nyu.edu>
2023-01-09 01:40:27 -0500

    fix low issues

--- quiz-app/src/API.ts
+++ quiz-app/src/API.ts
@@ -1,19 +1,21 @@
-import { Question, QuestionState, Difficulty } from "./Types";
-import { shuffleArray } from './util';
+import { Question, Difficulty } from './Types'
+import { shuffleArray } from './util'
 
-
-export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
+export const fetchQuizQuestions = async (
+  amount: number,
+  difficulty: Difficulty
+) => {
   // sample https://opentdb.com/api.php?amount=50&category=25&difficulty=easy&type=multiple
-  const endPoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
-  const data = await(await fetch(endPoint)).json();
+  const endPoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`
+  const data = await (await fetch(endPoint)).json()
   // console.log(endPoint);
   // console.log(data);
 
-  return data.results.map((question: Question) => (
-    {
-      ...question,
-      answers: shuffleArray([...question.incorrect_answers, question.correct_answer]),
-    }
-  ))
-
+  return data.results.map((question: Question) => ({
+    ...question,
+    answers: shuffleArray([
+      ...question.incorrect_answers,
+      question.correct_answer,
+    ]),
+  }))
 }
--- quiz-app/src/components/QuestionCard.styles.ts
+++ quiz-app/src/components/QuestionCard.styles.ts
@@ -33,19 +33,16 @@ export const ButtonWrapper = styled.div<ButtonWrapperProps>`
     width: 100%;
     height: 40px;
     margin: 5px 0;
-    background: ${({ correct, userClicked}) =>
+    background: ${({ correct, userClicked }) =>
       correct
         ? 'linear-gradient(90deg, #56ffa4, #59bc86)'
         : !correct && userClicked
         ? 'linear-gradient(90deg, #ff5656, #c16868)'
-        : 'linear-gradient(90deg, #56ccff, #6eafb4)'
-    };
+        : 'linear-gradient(90deg, #56ccff, #6eafb4)'};
     border: 3px solid #ffffff;
-    box-shadow: 1px 2px 0px rgba(0,0,0,0.1);
+    box-shadow: 1px 2px 0px rgba(0, 0, 0, 0.1);
     border-radius: 10px;
     color: #ffffff;
-    text-shadow: 0px 1px 0px rgba(0,0,0,0.25);
+    text-shadow: 0px 1px 0px rgba(0, 0, 0, 0.25);
   }
-
-
-`;
+`
